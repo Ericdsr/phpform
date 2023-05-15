987f802f6b79b8a4c282761e7032673450f0ba69
Ericdsr <eric62.dasilvarocha@laposte.net>
2023-05-15 12:34:11 +0200

    ahref bouton pas compris

--- php/index.php
+++ php/index.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-echo $url;
 ?>
 
 
@@ -17,6 +16,11 @@ echo $url;
 <?php
     include './includes/ul.inc.html';
 ?>
+  <section class="d-flex justify-content-center w-75 h-25 p-3">
+      <div class="btn-group " role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary" > <a href="./form.inc.html">Ajouter des données</a></button>
+      </div>
+  </section>
 <?php
     include './includes/footer.inc.html';
 ?>
