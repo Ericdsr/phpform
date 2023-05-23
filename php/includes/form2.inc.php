cd357ad3895df97bc357ae014e58c987dc8426c9
Ericdsr <eric62.dasilvarocha@laposte.net>
2023-05-23 15:37:04 +0200

    part 2 le COMMMENCEMENT

---
+++ php/includes/form2.inc.php
@@ -0,0 +1,78 @@
+<form action="index.php" method="POST" class="col-md-9">
+    <div class="mb-3" >
+      <label for="exampleInputEmail1" class="form-label">Prénom</label>
+      <input type="text" class="form-control" name="first_name">
+    </div>
+    <div class="mb-3" >
+        <label for="exampleInputEmail1" class="form-label">Nom</label>
+        <input type="text" class="form-control" name="last_name">
+      </div>
+      <div class="mb-3" >
+        <label for="exampleInputEmail1" class="form-label">Age (18ans à 70)</label>
+        <input type="number" class="form-control" min="18" max="70" name="age">
+      </div>
+      <div class="input-group" >
+          <span class="input-group-text">Taille(1,26m à 3m)</span>
+          <input type="number" class="form-control" step="0.01" min="1.26" max="3" name="size">
+        <span class="input-group-text">m</span>
+      </div>
+    <div class="mb-3 form-check" >
+      <input type="checkbox" class="form-check-input" id="exampleCheck1" name="civility" value="Mr">
+      <label class="form-check-label" for="exampleCheck1">homme</label>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="civility" value="Madame">
+        <label class="form-check-label" for="exampleCheck1">femme</label>
+      </div>
+    <div>
+        <h6> Connaisances</h6>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="html" value="html">
+        <label class="form-check-label" for="exampleCheck1">HTML</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="css" value="css">
+        <label class="form-check-label" for="exampleCheck1">CSS</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="javascript" value="javascript">
+        <label class="form-check-label" for="exampleCheck1">JavaScript</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="php" value="php">
+        <label class="form-check-label" for="exampleCheck1">PHP</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="mysql" value="mysql">
+        <label class="form-check-label" for="exampleCheck1">MySQL</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="bootstrap" value="bootstrap">
+        <label class="form-check-label" for="exampleCheck1">Bootstrap</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="symfony" value="sympfony">
+        <label class="form-check-label" for="exampleCheck1">Sympfony</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="react" value="react">
+        <label class="form-check-label" for="exampleCheck1">React</label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="color" name="color" value="color">
+        <label for="color"> Couleur preferer </label>
+      </div>
+      <div class="mb-3 form-check">
+        <input type="date" name="date" value="date">
+        <label for="date"> Date de naissance </label>
+      </div>
+      <div>
+        <h6> joindre une image</h6>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="file" name="file" value="file">
+        <label for="file"> Choisir un fichier </label>
+      </div>
+    <button type="submit" class="btn btn-primary" name="submit" value="Submit">Enregistrer les données</button>
+  </form>
--- php/index.php
+++ php/index.php
@@ -23,19 +23,45 @@ include './includes/header.inc.html';
     if (isset($_GET['add'])){
         include './includes/form.inc.html';
     }
+    else if (isset($_GET['addmore'])){
+        include './includes/form2.inc.php';
+    }
     else if(isset($_POST['submit'])){
         $prenom = $_POST['first_name'];
         $nom = $_POST['last_name'];
         $age = $_POST['age'];
         $size = $_POST['size'];
         $civility = $_POST['civility'];
+        $html = $_POST['html'];
+        $css = $_POST['css'];
+        $javascript = $_POST['javascript'];
+        $php = $_POST['php'];
+        $mysql = $_POST['mysql'];
+        $bootstrap = $_POST['bootstrap'];
+        $symfony = $_POST['symfony'];
+        $react = $_POST['react'];
+        $color = $_POST['color'];
+        $date = $_POST['date'];
+        $file = $_POST['file'];
 
+        
         $table = array(
             'first_name' => $prenom,
             'last_name' => $nom,
             'age' => $age,
             'size' => $size,
             'civility' => $civility,
+            'html' => $html,
+            'css' => $css,
+            'javascript' => $javascript,
+            'php' => $php,
+            'mysql' => $mysql,
+            'bootstrap' => $bootstrap,
+            'symfony' => $symfony,
+            'react' => $react,
+            'color' => $color,
+            'date' => $date,
+            'file' => $file,
         );
         $_SESSION['table'] = $table;
         echo "données save";
@@ -84,6 +110,7 @@ include './includes/header.inc.html';
     }
     else {
         echo "<div class='col-md-9'><a href='index.php?add' class='btn btn-primary'> Ajouter des données</a></div>";
+        echo "<div class='col-md-9'><a href='index.php?addmore' class='btn btn-primary'> Ajouter Plus de données</a></div>";
     }
     function readTable($table){
         $loop = 0;
